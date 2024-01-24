d91458764abad6e529432285ae53fd824cd68cf1
I. Safari <isafari@luc.edu>
2024-01-24 00:03:49 +0000

    Finished WarmUp_3.

--- WarmUp_3/warm3_args.c
+++ WarmUp_3/warm3_args.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+    //Objective: Write a program to initialize an array of integers and find the sum and average of the elements.
 int main(){
-
+    //initialize array of numbers
+    int numbers[] = {1,2,3,4,5};
+    //init a sum
+    int sum = 0;
+    //loop through each number of it
+    int i;
+    for(i = 0; i <= sizeof(numbers)/sizeof(numbers[0]); i++){
+        sum+=numbers[i];
+    }
+    //add the number to the sum 
+    double average = sum / ((double) sizeof(numbers)/sizeof(numbers[0]));
+    //initialize 'double average' to be sum divided by the numbers length (double)
+    
+    //print out 'sum:' and print out 'average:' (but sum and avg inside the printfs)
+    printf("Sum is: %i\n", sum);
+    printf("Avg is: %1f\n", average);
 }
+/*
+*/
+/*
+public class ArrayOperations {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        double average = sum / (double) numbers.length;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+    }
+}
+*/
