78d22581af189a31cb46faa9f232fb716f447f42
I. Safari <isafari@luc.edu>
2024-01-28 01:56:22 +0000

    Finished WarmUp_7

--- WarmUp_7/warm7_args.c
+++ WarmUp_7/warm7_args.c
@@ -2,15 +2,17 @@
 
 int linearSearch(int arr[], int size, int target);
 
+
 int main(){
     int array[] = {1,4,7,9,12};
     int target = 9;
     int index = linearSearch(array, sizeof(array)/sizeof(array[0]), target);
-if (index != -1) {
-    printf("Element found at index: %d\n", index);
-} else {
-    printf("Element not found\n");
-}
+    if (index != -1) {
+        printf("Element found at index: %d\n", index);
+    } else {
+        printf("Element not found\n");
+    }
+    }
 int linearSearch(int arr[], int size, int target) {
 for (int i = 0; i < size; i++) {
     if (arr[i] == target) {
@@ -20,4 +22,3 @@ for (int i = 0; i < size; i++) {
 return -1;
 }
 
-}
