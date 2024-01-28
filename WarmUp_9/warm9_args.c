09914d4a71ab4249e0da397304583f48e85b1672
I. Safari <isafari@luc.edu>
2024-01-28 19:09:17 +0000

    Finished WarmUp_9

--- WarmUp_9/warm9_args.c
+++ WarmUp_9/warm9_args.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
-
-
-
+/*Objective: Write a program to create a file, 
+ write some data into it, 
+ then read the data from the file.
+ */
 int main(){
-   
+
+const char* filename = "example.txt";
+//create a file
+FILE* fp = fopen(filename, "w+");
+//if success, else error and return 1
+if(!fp){printf("No file");}
+    return 1;
+
+   //write data into the file fp
+fputs("Hello, world!\n", fp);
+//beginning
+rewind(fp);
+
+int br;
+//read and put out contents to console
+while((br = fgetc(fp)) != EOF){
+    putchar(br);
+}
+fclose(fp);
+remove(filename);
 }
 
